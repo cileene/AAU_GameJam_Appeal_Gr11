05b88b49b8a5aa6471ea64d18e9406f9467477b0
ValdemarWessing <iq62FM@student.aau.dk>
2024-11-26 13:56:05 +0100

    add Jump

--- Assets/Scripts/Character_Controler.cs
+++ Assets/Scripts/Character_Controler.cs
@@ -3,24 +3,52 @@ using UnityEngine;
 public class Character_Controler : MonoBehaviour
 {
     public float moveSpeed = 1f;
-    //public float jumpForce = 3f;
+    public float jumpForce = 5f;
     private Rigidbody _rb;
     private float hInput;
     private Vector3 mDirection;
+    private bool jumpKey;
+    private bool isGrounded;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
-    // vi burger FixedUpdate for at 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jumpKey = true;
+        }
+    }
+
     void FixedUpdate()
     {
+        if (jumpKey)
+        {
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+            jumpKey = false;
+        }
+
         hInput = Input.GetAxis("Horizontal");
-
         mDirection = new Vector3(hInput, 0, 0) * moveSpeed;
-
         _rb.AddForce(mDirection, ForceMode.VelocityChange);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }

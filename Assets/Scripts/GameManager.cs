7d47aad839b0c6d7e55f0c325d7f846a3b68b61d
ValdemarWessing <iq62FM@student.aau.dk>
2024-11-28 12:39:10 +0100

    implement end screen and restart button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static int angryJudge = 0;
     public static int Score = 0;
+    public GameObject GameOverScreen;
+    public GameObject AppealText;
+    public TextMeshProUGUI scoreText;
+    public GameObject NormalFace;
+    public GameObject StartAngryFace;
+    public GameObject MidAngryFace;
+    public GameObject EndAngryFace;
+
 
     void Update()
     {
-        if (angryJudge >= 3)
+        if (angryJudge == 3)
         {
-            GameOver();
+            GameOverScreen.SetActive(true);
+            scoreText.text = $"Your score was: {Score}";
+            AppealText.SetActive(false);
+            Time.timeScale = 0;
+        }
+        if (angryJudge == 0)
+        {
+            NormalFace.SetActive(true);
+        }
+        if (angryJudge == 1)
+        {
+            StartAngryFace.SetActive(true);
+            NormalFace.SetActive(false);
+        }
+        if (angryJudge == 2)
+        {
+            MidAngryFace.SetActive(true);
+            StartAngryFace.SetActive(false);
+        }
+        if (angryJudge == 3)
+        {
+            EndAngryFace.SetActive(true);
+            MidAngryFace.SetActive(false);
         }
     }
     public static void GameOver() //TODO: implement game over
@@ -23,4 +54,16 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene("GameStart"); //TODO: implement game start
         Debug.Log("Game Start");
     }
+    public void restartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("codeTestScene");
+        GameOverScreen.SetActive(false);
+        AppealText.SetActive(true);
+        angryJudge = 0;
+        Score = 0;
+    }
+
+
+
 }
---
+++ Assets/Scripts/TryAgain.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+public class TryAgain : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    void Start()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("codeTestScene");
+        
+    }
+}
+*/
